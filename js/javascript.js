4af9f831b5021fc79ebd05c83cea87f7fb2b0718
jakubstellner <jakub.stellner03@gmail.com>
2021-10-02 17:41:25 +0200

    map

---
+++ js/javascript.js
@@ -0,0 +1,98 @@
+$(document).ready(function () {
+
+    function thisClass(element, defaults = 'active') {
+      if ($(element).hasClass(defaults)) {
+        $(element).removeClass(defaults);
+      } else {
+        $(element).addClass(defaults);
+      }
+    }
+
+    function navsCarousel(element, nav) {
+      $(nav+'.prev').click(function(e) {
+        e.preventDefault();
+        element.trigger('prev.owl.carousel');
+      })
+
+      $(nav+'.next').click(function(e) {
+        e.preventDefault();
+          element.trigger('next.owl.carousel');
+      })
+    }
+
+
+    //CAROUSELS
+    const homepage__slider = $('.homepage__slider');
+    homepage__slider.owlCarousel({
+      loop:false,
+      nav:false,
+      items: 1,
+      dots: true
+    })
+
+    const news__slider = $(".news__slider");
+    news__slider.owlCarousel({
+      loop: true,
+      nav:false, 
+      dots: false,
+      margin: 30,
+      items:1,
+      responsive: {
+        776: {
+          items:2
+        },
+        1200: {
+          items: 4
+        }
+      }
+    })
+
+    navsCarousel(news__slider, ".news .carousel-nav");
+
+    const product__slider = $('.product__slider');
+    product__slider.owlCarousel({
+      loop: true,
+      items:1,
+      dots: false,
+      nav: false,
+      margin: 30,
+      responsive: {
+        991: {
+          items: 2
+        },
+        1200: {
+          items: 3
+        }
+      }
+    })
+
+    navsCarousel(product__slider, ".products .carousel-nav");
+
+    const reference__slider = $('.reference__slider');
+    reference__slider.owlCarousel({
+      loop: true,
+      items: 1,
+      dots: false,
+      nav: false,
+      margin: 30,
+      responsive: {
+        576: {
+          items: 2
+        },
+        991: {
+          items: 4
+        },
+        1200: {
+          items: 6
+        },
+        1500: {
+          items: 7
+        }
+      }
+    })
+
+    navsCarousel(reference__slider, '.reference .carousel-nav')
+
+  
+
+});
